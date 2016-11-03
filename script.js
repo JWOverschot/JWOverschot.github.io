1a3bb5717667111f2deaa3fccfb8eccdc99e5b6d
Jis van Overschot <99040091@mydavinci.nl>
2016-11-03 09:24:36 +0100

    flex added, and mose over text.

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
-
-
-var status = 1;
+var action = 1;
 
 function changeColor() {
-var x = document.getElementById("bg");
-var y = document.getElementById("fontcolor");
+	var x = document.getElementById("bg");
+	var y = document.getElementById("fontcolor");
 
-if(status==1) {
-	x.style.backgroundColor = "black";
-    y.style.filter = "invert(100%)";
-    status = 2;
-}
-else if(status==2) {
-	x.style.backgroundColor = "white";
-    y.style.filter = "invert(0%)";
-    status = 1;
+	if(action==1) {
+		x.style.backgroundColor = "black";
+	    y.style.filter = "invert(100%)";
+	    action = 2;
+	}
+	else if(action==2) {
+		x.style.backgroundColor = "white";
+	    y.style.filter = "invert(0%)";
+	    action = 1;
+	}
 }
 
+function mOverGit() {
+	console.log ("true")
+	document.getElementById("text").innerHTML = "Github";
+}
+function mOverMail() {
+	console.log ("true")
+	document.getElementById("text").innerHTML = "Mail";
+}
+function mOverInfo() {
+	console.log ("true")
+	document.getElementById("text").innerHTML = "Info";
+}
+function mOverBook() {
+	console.log ("true")
+	document.getElementById("text").innerHTML = "Book";
+}
+function mOverFolder() {
+	console.log ("true")
+	document.getElementById("text").innerHTML = "Folder";
+}
+function mOut() {
+	document.getElementById("text").innerHTML = "Jis van Overschot";
 }
